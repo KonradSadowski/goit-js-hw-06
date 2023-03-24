561247ade04a138f4ec0233f26b91173e1023807
KonradSadowski <darnokowsky@gmail.com>
2023-03-24 20:10:27 +0100

    Zrobione zadania 5-8

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -2,5 +2,12 @@ const inputEl = document.querySelector(`#name-input`);
 const outputEl = document.querySelector(`#name-output`);
 
 inputEl.addEventListener('input', event => {
-  outputEl.textContent = event.currentTarget.value || 'Anonymous';
+  if (inputEl.value !== '') {
+    outputEl.textContent = event.currentTarget.value;
+  } else {
+    outputEl.textContent = 'Anonymous';
+  }
 });
+// inputEl.addEventListener('input', event => {
+//   outputEl.textContent = event.currentTarget.value || 'Anonymous';
+// });
