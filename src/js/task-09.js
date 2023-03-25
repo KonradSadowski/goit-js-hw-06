1d93938af113eb721a33d4f4139b2db858d0997d
KonradSadowski <darnokowsky@gmail.com>
2023-03-25 16:58:27 +0100

    obowiązkowe zadania wykonane

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,14 @@
+const buttonEl = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+const spanEl = document.querySelector('.color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+buttonEl.addEventListener('click', () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+});
